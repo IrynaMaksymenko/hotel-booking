cb45cf12b5da8ca6f012a974b80c4053feb2249d
Irina Maximenko <Irina.Maximenko@dataart.com>
2017-05-07 13:22:50 +0300

    added logging

--- src/main/java/com/exercises/hotelbooking/controllers/HotelBookingController.java
+++ src/main/java/com/exercises/hotelbooking/controllers/HotelBookingController.java
@@ -1,6 +1,8 @@
 package com.exercises.hotelbooking.controllers;
 
 import com.exercises.hotelbooking.models.Hotel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +15,14 @@ import static java.util.Arrays.asList;
 @RestController
 public class HotelBookingController {
 
+    private static final Logger log = LoggerFactory.getLogger(HotelBookingController.class);
+
     /**
      * Get all hotels of a current city.
      */
     @RequestMapping(value = "/hotels", method = RequestMethod.GET)
     public Collection<Hotel> getHotels(@RequestParam String city) {
+        log.debug("Retrieving hotels in {}", city);
         final Hotel hotel = new Hotel();
         hotel.setCity(city);
         return asList(hotel);
---
+++ src/main/java/com/exercises/hotelbooking/health/ApplicationHealthIndicator.java
@@ -0,0 +1,16 @@
+package com.exercises.hotelbooking.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        // TODO: improve check
+        return Health.up().build();
+    }
+
+}
